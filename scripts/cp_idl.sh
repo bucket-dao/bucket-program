69cc36bc6bab088bf5f34d8cada8c300ca32da5f
aletna <dennis.antela@gmail.com>
2022-02-26 11:58:05 -0700

    use binaries

--- scripts/cp_idl.sh
+++ scripts/cp_idl.sh
@@ -1,8 +1,8 @@
 # ------- copy IDL and types
 # IDL to app
-cp ./target/idl/bucket_program.json ./app/public/
+cp ./target/idl/bucket_program.json ./app/src/types/
 
 # ------- types to SDK
-cp -r target/types ./src
+cp -r target/types ./sdk/src
 
 echo IDLs and Types copied ✅
