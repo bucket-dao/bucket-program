90cf257324e9e29e925ac6018b0491cd0bde8d19
jshiohaha <dev@jacobshiohira.com>
2022-02-26 17:07:12 -0700

    Update deploy script to default to upgrade program instead of deploying a new copy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,19 @@
-if [ $# -gt 1 ]; then
-    echo "Cluster name is the only optional accepted parameter: devnet, mainnet-beta"
+if [ $# -eq 0 ]; then
+    echo "Must provide at least 1 param to specify if script should update programId"
     exit 1
 fi
 
+if [ $# -eq 1 ]; then
+    echo "Must provide at least 1 param to specify if program is upgraded or deployed"
+    exit 1
+fi
+update_flag=$1
+
 network='devnet' # set default if param not specified
-if [ $# -ne 0 ]; then
-    if [ "$1" = devnet ]; then
+if [ $# -eq 2 ]; then
+    if [ "$2" = devnet ]; then
         network='devnet'
-     elif [ "$1" = mainnet-beta ]; then
+     elif [ "$2" = mainnet-beta ]; then
         network='mainnet-beta'
     else
         echo "devnet, mainnet-beta are the only acceptable cluster names"
@@ -17,26 +23,32 @@ fi
 
 echo "Deplying to $network";
 
-# fetch old pk
-old_bucket_pk=`solana-keygen pubkey ./target/deploy/bucket_program-keypair.json`
-echo Old public key: $old_bucket_pk
+# fetch existing pk
+existing_bucket_pk=`solana-keygen pubkey ./target/deploy/bucket_program-keypair.json`
+echo Existing public key: $existing_bucket_pk
 
-# stash old keypair
-cd ./target/deploy # need to cd for renaming to work ok
-mv bucket_program-keypair.json bucket_program-keypair-`ls | wc -l | xargs`.json
-cd ./../..
+# if we do not upgrade, we will deploy a new copy of the program
+if [ $update_flag -ne 1 ]; then
+    # stash existing keypair
+    cd ./target/deploy # need to cd for renaming to work ok
+    mv bucket_program-keypair.json bucket_program-keypair-`ls | wc -l | xargs`.json
+    cd ./../..
 
-# build and fetch new pk
-anchor build
-new_bucket_pk=`solana-keygen pubkey ./target/deploy/bucket_program-keypair.json`
-echo New public key: $new_bucket_pk
+    # build and fetch new pk
+    anchor build
+    new_bucket_pk=`solana-keygen pubkey ./target/deploy/bucket_program-keypair.json`
+    echo New public key: $new_bucket_pk
 
-targets=$(find . -type f  -exec grep -lir --include=*.{ts,tsx,rs,toml} $old_bucket_pk {} +)
-for target in $targets
-do
-    sed -i'.original' -e "s/$old_bucket_pk/$new_bucket_pk/g" $target
-done
-echo Public key replaced!
+    targets=$(find . -type f  -exec grep -lir --include=*.{ts,tsx,rs,toml} $existing_bucket_pk {} +)
+    for target in $targets
+    do
+        echo 'Replacing pk in ' $target
+        # sed -i'.original' -e "s/$existing_bucket_pk/$new_bucket_pk/g" $target
+    done
+    echo Public key replaced!
+else
+    echo Using existing public key: $existing_bucket_pk
+fi
 
 # build again with new pk
 anchor build
